43bb5c0c8de72a7395e82829648096c01a0a2efa
vladbil <vlad00119@gmail.com>
2017-05-31 10:49:26 +0300

    lab1
    com1

---
+++ Assets/Content/C#Scripts/HeroRabit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabit : MonoBehaviour {
+	public float speed = 1;
+	Rigidbody2D myBody = null;
+	// Use this for initialization
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//[-1, 1]
+		float value = Input.GetAxis ("Horizontal");
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+
+		//повертання кролика  
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		if(value < 0) {
+			sr.flipX = true;
+		} else if(value > 0) {
+			sr.flipX = false;
+		}
+	}
+
+}
