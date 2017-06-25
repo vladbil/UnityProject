eda00eeddedde1270678acb7254c202f5afcffb6
vladbil <vlad00119@gmail.com>
2017-06-25 22:42:55 +0300

    Lab 3
    commit for third lab

---
+++ Assets/Content/C#Scripts/Y_MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Y_MovingPlatform : MonoBehaviour {
+
+    private Vector3 startPosition;
+   
+    private Vector3 newPosition;
+
+  
+    [SerializeField]
+    public int speed = 1;
+   
+    [SerializeField]
+    public int maxDistance = 2;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        newPosition = transform.position;
+    }
+
+    void Update()
+    {
+        newPosition.y = startPosition.y + (maxDistance * Mathf.Sin(Time.time * speed));
+        transform.position = newPosition;
+    }
+}
